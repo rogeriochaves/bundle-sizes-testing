5b9b9ba2d33dbb2eb3e2b61e821e06dfc605769e
Rogerio Chaves <rogeriochaves@thoughtworks.com>
2016-12-29 10:29:13 -0200

    Add react and elm examples

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: {
+    react: './react.js',
+    elm: './elm.js',
+    reactComplete: './react-complete.js'
+  },
+  output: {
+    path: `${__dirname}/build`,
+    filename: '[name].[hash].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        loader: 'elm-webpack-loader'
+      }
+    ]
+  }
+};
