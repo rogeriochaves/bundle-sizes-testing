f695bd3d74c6b9ed9a80f05653f06f3c4d18388f
Rogerio Chaves <rogeriochaves@thoughtworks.com>
2016-12-29 12:02:47 -0200

    Add react-complete example

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,11 @@ module.exports = {
         test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
         loader: 'elm-webpack-loader'
+      },
+      {
+        test: /react-complete.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
     ]
   }
